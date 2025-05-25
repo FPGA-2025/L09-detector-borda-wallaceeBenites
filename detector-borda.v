4010ab9c05d505702963b630acb01f5d48fea52c
wallaceeBenites <wallacepuck@gmail.com>
2025-05-25 16:10:12 -0300

    teste

--- detector-borda.v
+++ detector-borda.v
@@ -2,18 +2,18 @@ module detector_borda (
     input clk,
     input rst,
     input [1:0] entrada,
-    output [1:0] detector
+    output reg [1:0] detector
 );
     reg [1:0] entrada_reg; 
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
             entrada_reg <= 2'b00;
+            detector <= 2'b00;
         end else begin
             entrada_reg <= entrada;
+            detector <= entrada & ~entrada_reg;
         end
     end
 
-    
-    assign detector = entrada & ~entrada_reg;
 endmodule
